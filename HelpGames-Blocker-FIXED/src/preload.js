6c934310f60d52683e92f5bfb341c5e9d9587800
Emerson Santos <60586125+elbsantos@users.noreply.github.com>
2026-04-12 08:15:54 +0100

    Delete HelpGames-Blocker-FIXED/src directory

--- HelpGames-Blocker-FIXED/src/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  login:            (credentials) => ipcRenderer.invoke('login', credentials),
-  openWebDashboard: () => ipcRenderer.invoke('open-web-dashboard'),
-});
